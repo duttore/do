ba3a32e66f31cca15f82a3a5a98bb141818d9a17
levvser <vval.mail@icloud.com>
2024-10-26 15:48:56 +0200

    +

--- .history/tailwind.config_20241026145310.ts
+++ .history/tailwind.config_20241026145310.ts
@@ -2,11 +2,7 @@ import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}", // Ensure you cover all potential file extensions used in your components
-    "./pages/**/*.{js,ts,jsx,tsx}", // Include pages directory if applicable
-    "./components/**/*.{js,ts,jsx,tsx}", // Include components directory
-  ],
+  content: ["./src/**/*.tsx"],
   theme: {
     extend: {
       fontFamily: {
